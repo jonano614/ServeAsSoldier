31b4d99b45a9592c9e32ecb3cf297e23bcdbdd8e
Evgeniy <jonano614@mail.ru>
2023-03-14 21:46:16 +0300

    VS solution, some assembly references are fixed.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("ServeAsSoldier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServeAsSoldier")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9992f584-5ec9-4530-bdba-a9fe5e7629f4")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
